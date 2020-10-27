266e64a57b0806c2b7c69e0d51099eb31ee93401
Li-Wei Wang <Liwei_Wang@asus.com>
2020-10-27 14:20:12 +0800

    Add.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     std::cout << "Hello! AICS!" << std::endl;
     std::cout << "Hello! AICS!" << std::endl;
+    std::cout << "Hello! AICS!" << std::endl;
     return 0;
 }
